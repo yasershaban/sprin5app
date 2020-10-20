25ca2a4c3406d9eb6750ef8e7db2c40bb7ed21b6
Yaser Shaban <yaserbshaban@gmail.com>
2020-10-20 16:34:12 +0200

    first commit

---
+++ src/main/java/com/course/spring5app/Spring5appApplication.java
@@ -0,0 +1,13 @@
+package com.course.spring5app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/spring5app/Spring5appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.spring5app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
