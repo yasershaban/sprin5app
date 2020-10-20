724deb50cf7fceb901954ce96f84eb2969e7d83b
Yaser Shaban <yaserbshaban@gmail.com>
2020-10-20 17:01:15 +0200

    extend jpa

--- src/main/java/com/course/spring5app/Repos/AuthorRepo.java
+++ src/main/java/com/course/spring5app/Repos/AuthorRepo.java
@@ -1,5 +1,9 @@
 package com.course.spring5app.Repos;
 
-public interface AuthorRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.spring5app.modell.Author;
+
+public interface AuthorRepo extends JpaRepository<Author, Long> {
 
 }
--- src/main/java/com/course/spring5app/Repos/BookRepo.java
+++ src/main/java/com/course/spring5app/Repos/BookRepo.java
@@ -1,5 +1,9 @@
 package com.course.spring5app.Repos;
 
-public interface BookRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.spring5app.modell.Book;
+
+public interface BookRepo extends JpaRepository<Book, Long >{
 
 }
